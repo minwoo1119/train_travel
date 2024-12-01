188dacfca93be376325bb22fbdb24cd4b83de963
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-12-01 20:03:49 +0900

    ✨ random 지역 홍보 컴포넌트 생성

---
+++ src/components/advertisement/Advertisement.jsx
@@ -0,0 +1,15 @@
+import styles from './advertisement.module.css';
+
+const Advertisement = ({ photoUrl, regionName, regionScript }) => {
+	return (
+		<div className={styles.container}>
+			<div className={styles.regionImg}>photo</div>
+			<div className={styles.regionInfo}>
+				<div className={styles.regionName}>{regionName}</div>
+				<div className={styles.regionScript}>{regionScript}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Advertisement;
