fe1255ac0c877c415602634f5631178d0705a20a
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-12-01 19:27:30 +0900

    ✨ react-router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
+import Header from './components/header/header';
 import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<>
-			<MainPage />
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<MainPage />}></Route>
+					<Route path="/search" element={<SearchPage />}></Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
