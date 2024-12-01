d4c18e2572404e7004b7f2fa07f7a89f62826ff4
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-12-01 20:04:33 +0900

    ✨ 메인 페이지 초기 구성

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,36 @@
-import Header from '../components/header/header';
+import RegionInfo from '../components/regioninfo/RegionInfo';
 import styles from './mainpage.module.css';
+import { Link } from 'react-router-dom';
 
 const MainPage = () => {
 	return (
 		<div className={styles.container}>
-			<Header />
+			<div className={styles.titletext}>기차를 타고 어디로 떠나볼까요 ?</div>
+			<Link className={styles.btn} to="search">
+				<div className={styles.searchBtn}>찾는 지역이 있으신가요 ?</div>
+			</Link>
+			<div className={styles.regionCard}>
+				<RegionInfo
+					regionName="서울특별시"
+					regionEngName="Seoul"
+					regionImg=""
+				/>
+				<RegionInfo
+					regionName="서울특별시"
+					regionEngName="Seoul"
+					regionImg=""
+				/>
+				<RegionInfo
+					regionName="서울특별시"
+					regionEngName="Seoul"
+					regionImg=""
+				/>
+				<RegionInfo
+					regionName="서울특별시"
+					regionEngName="Seoul"
+					regionImg=""
+				/>
+			</div>
 		</div>
 	);
 };
